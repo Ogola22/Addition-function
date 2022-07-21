326ce78b58b096603132292afb9cefbb2fe3662c
Brian <brianochiengogola78@gmail.com>
2022-07-21 17:18:40 +0300

    Addition function

---
+++ add.js
@@ -0,0 +1,5 @@
+function AddNumbers(){
+    let num1 =parseInt (document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+    document.getElementById('sum').value = num1 + num2;
+}
